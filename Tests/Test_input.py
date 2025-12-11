0afb93654b9fd6017991b4b1a95bfe6bdbf0c0e2
Kumar4nil15 <Anilkdyadav15@gmail.com>
2025-12-11 15:49:09 +0530

    Update and rename framework to Tests/Test_input.py

---
+++ Tests/Test_input.py
@@ -0,0 +1,27 @@
+from utils.driver_setup import start_browser
+from pages.home_page import HomePage
+from pages.result_page import ResultPage
+import time
+
+
+def test_valid_input():
+    driver = start_browser()
+
+    try:
+        home = HomePage(driver)
+        result = ResultPage(driver)
+
+        home.open()
+        home.enter_text("abcabcbb")
+        home.click_submit()
+
+        time.sleep(1)   # small wait
+
+        output = result.get_result()
+
+        assert output == "3"
+
+    finally:
+        driver.quit()
+
+
