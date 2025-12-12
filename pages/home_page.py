75ef77a6243dbeff979780af1ba3dc56f8219fb9
Kumar4nil15 <Anilkdyadav15@gmail.com>
2025-12-12 08:06:01 +0530

    Update home_page.py

--- pages/home_page.py
+++ pages/home_page.py
@@ -1,1 +1,16 @@
+from locators.home_locators import HomeLocators
+
+class HomePage:
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def open(self):
+        self.driver.get("https://agrichain.com")
+
+    def enter_text(self, text):
+        self.driver.find_element(*HomeLocators.INPUT).send_keys(text)
+
+    def click_submit(self):
+        self.driver.find_element(*HomeLocators.SUBMIT).click()
 
